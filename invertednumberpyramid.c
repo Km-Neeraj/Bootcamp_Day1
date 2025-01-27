7162115afe249dbcde19bd3302af4e9407171535
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-27 21:22:32 +0530

    Add files via upload

---
+++ invertednumberpyramid.c
@@ -0,0 +1,25 @@
+     #include<stdio.h>
+   int main()
+   {
+        int i,j,row;
+        printf("Enter the number of row\n");
+        scanf("%d",&row);
+        for(i=row;i>=1;i--)
+        {
+             for(j=row;j>i;j--)
+             {
+                printf(" ");
+             }
+             for(j=1;j<=i;j++)
+             {
+                printf("%d",j);
+             }
+             for(j=i-1;j>=1;j--)
+             {
+               printf("%d",j);
+             }
+             printf("\n");
+
+        }
+        return 0;
+   }
