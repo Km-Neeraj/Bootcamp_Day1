9b1777aa200b563a979342a1c7611d4d09245b4a
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-27 21:24:11 +0530

    Add files via upload

---
+++ numbertriangle.c
@@ -0,0 +1,18 @@
+     
+        #include<stdio.h>
+        int main()
+        {
+              int i,j,row;
+              printf("Enter the number of row\n");
+              scanf("%d",&row);
+              for(i=1;i<=row;i++)
+              {
+                   for(j=1;j<=i;j++)
+                   {
+                       printf("%d",j);
+                   }
+                   printf("\n");
+              }
+              return 0;
+              
+        }
